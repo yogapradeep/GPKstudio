adef39196adf57f85704aeb476194bd5d030ae95
pradeep <v.yogapradeep@gmail.com>
2020-03-01 12:12:54 +0530

    added style

--- admin/templates/works.js
+++ admin/templates/works.js
@@ -6,11 +6,11 @@ const html = htm.bind(h);
 const works = createClass({
 
   render() {
-    const mystyle = {
-      color: "green",
-      backgroundColor: "DodgerBlue",
-      padding: "10px",
-      fontFamily: "Arial"
+    const eimg = {
+
+      width: 200,
+      height: 400,
+
     };
 
     const entry = this.props.entry;
@@ -29,7 +29,7 @@ const works = createClass({
         ${entry.getIn(["data", "events"], []).map((evt, i) => html`
            <div class="col-md-6  col-lg-4 mb-4 m-2 " >
             <div class="blog_entry">
-            <img src=${getAsset(evt.get("image1"))} alt="Image" />
+            <img  src=${getAsset(evt.get("image1"))} alt="Image" style="${eimg}" />
               <div class="p-4 bg-white">
                 <h3 class="date">${evt.get("date")}</h3>
                <h5> ${evt.get("location")}</h5>
